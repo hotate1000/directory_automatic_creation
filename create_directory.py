bd6b63924a173f5fde7d0872c3811d547c66edfe
hotate1000 <ooo.336.sss@gmail.com>
2022-05-22 01:28:55 +0900

    ファイル自動作成機能作成

--- create_directory.py
+++ create_directory.py
@@ -4,6 +4,8 @@ import tkinter.filedialog as fd
 import tkinter.messagebox as mb
 import tkinter.simpledialog as sd
 import datetime
+import sys
+import traceback
 
 
 tk.Tk().withdraw()
@@ -11,7 +13,7 @@ tk.Tk().withdraw()
 
 # プロジェクト名の記載
 def project_name_description():
-    project_name = sd.askstring("プロジェクト名を入力","プロジェクト名を入力してください。\nディレクトリ名に利用します。")
+    project_name = sd.askstring("プロジェクト名を入力", "プロジェクト名を入力してください。\nディレクトリ名に利用します。")
     return project_name
 
 
@@ -19,6 +21,11 @@ def project_name_description():
 def create_directory_name():
     today = datetime.datetime.now().strftime("%Y%m%d")
     project_name = project_name_description()
+    if project_name is None:
+        sys.exit()  # ディレクトリ選択画面でキャンセル選択
+    if project_name == "":
+        mb.showinfo("警告", "プロジェクト名を選択してください")  # プロジェクト名が未記載だった場合
+        create_directory_name()
     directory_names = [
         "接続情報",
         "契約資料",
@@ -47,40 +54,38 @@ def create_directory_name():
 def select_directory():
     filepath = fd.askdirectory(
         title="フォルダを作成するディレクトリを選択する",
-        initialdir = "C:"
+        initialdir="C:"
     )
+    print(filepath)
+    if filepath == "" or filepath is None:
+        sys.exit()  # ファイルパスの選択でキャンセス
     return filepath
-
-
-# def array_processing_directory(n):
-#     if isinstance(n, list):
-#         for i in n:
-            
 
 
 # ディレクトリの作成
 def create_directory():
-    # try:
-    directory_name = create_directory_name()
-    filepath = select_directory()
-    # os.makedirs(filepath + "/" + i)
-    for i in directory_name:
-        # os.mkdir(i)
-        if isinstance(i, list):
-            for j in i:
-                if isinstance(j, list):
-                    for k in j:
-                        print((filepath + "/" + str(i) + "/" + str(j) + "/" + str(k)))
-                else:
-                    print((filepath + "/" + str(i) + "/" + str(j)))
-        else:
-            # print("i"+str(i))
-            print((filepath + "/" + str(i)))
-    # except:
-    #     mb.showinfo("エラー","対象ファイル名が既に存在している可能性があります。\n不明箇所は開発者に問合せください。")
+    try:
+        directory_names = create_directory_name()
+        filepath = select_directory()
+        for i in directory_names:
+            if not isinstance(i, list):
+                d_i = i
+            if isinstance(i, list):
+                for j in i:
+                    if not isinstance(j, list):
+                        d_j = j
+                    if isinstance(j, list):
+                        for k in j:
+                            os.makedirs(filepath + "/" + str(d_i) + "/" + str(d_j) + "/" + str(k), exist_ok=False)
+                    else:
+                        os.makedirs(filepath + "/" + str(d_i) + "/" + str(d_j), exist_ok=False)
+            else:
+                os.makedirs(filepath + "/" + str(d_i), exist_ok=False)
+        fd.askopenfilename(initialdir=filepath)
+    except Exception as e:
+        mb.showinfo("エラー", str(e))
+        print(traceback.format_exc())
 
 
 if __name__ == '__main__':
     create_directory()
-
-
